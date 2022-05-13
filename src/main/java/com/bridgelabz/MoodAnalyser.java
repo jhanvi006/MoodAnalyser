e60a08b9fba2bab1cf8eb8ff88c8a9f727e84684
jhanvi006 <jhanvikankhara06@gmail.com>
2022-05-13 20:54:36 +0530

    Added: test case for happy and sad mood added

--- src/main/java/com/bridgelabz/MoodAnalyser.java
+++ src/main/java/com/bridgelabz/MoodAnalyser.java
@@ -5,8 +5,9 @@ public class MoodAnalyser {
     public String moodAnalyse(String message) {
         if (message.contains("sad")){
             return "SAD";
-        }
-        else
+        } else if (message.contains("happy")) {
+            return "HAPPY";
+        } else
             return null;
     }
 }
--- src/test/java/com/bridgelabz/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyserTest.java
@@ -11,4 +11,11 @@ public class MoodAnalyserTest {
         System.out.println(result);
         Assertions.assertEquals("SAD", result);
     }
+    @Test
+    public void givenMessage_WhenProper_ShouldReturnHappy() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String result = moodAnalyser.moodAnalyse("User is happy.");
+        System.out.println(result);
+        Assertions.assertEquals("HAPPY", result);
+    }
 }
