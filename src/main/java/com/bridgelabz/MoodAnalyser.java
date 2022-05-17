8eab028bbb6c4f07f8838531a2f1c0593f2be77d
jhanvi006 <jhanvikankhara06@gmail.com>
2022-05-17 21:29:55 +0530

    Added: custom exception class added

--- src/main/java/com/bridgelabz/MoodAnalyser.java
+++ src/main/java/com/bridgelabz/MoodAnalyser.java
@@ -2,20 +2,20 @@ package com.bridgelabz;
 
 public class MoodAnalyser {
     public static String message;
-    public MoodAnalyser(String message){
+    public MoodAnalyser(String message) throws MoodAnalyserException {
         this.message = message;
         moodAnalyse();
     }
-    public String moodAnalyse() {
+    public String moodAnalyse() throws MoodAnalyserException {
         try {
-            if (message.contains("Sad")) {
+            if (message.toLowerCase().contains("sad"))
                 return "SAD";
-            } else {
+            else
                 return "HAPPY";
-            }
         }
         catch (NullPointerException e) {
-            return "Happy";
+            System.out.println(e.getMessage());
+            throw new MoodAnalyserException("Entered invalid mood.");
         }
     }
 }
--- src/test/java/com/bridgelabz/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyserTest.java
@@ -5,38 +5,49 @@ import org.junit.jupiter.api.Test;
 
 public class MoodAnalyserTest {
     @Test
-    public void givenMessage_WhenProper_ShouldReturnSad() {
+    public void givenMessage_WhenProper_ShouldReturnSad() throws MoodAnalyserException {
         MoodAnalyser moodAnalyser = new MoodAnalyser("User is Sad.");
         String result = moodAnalyser.moodAnalyse();
         System.out.println(result);
         Assertions.assertEquals("SAD", result);
     }
     @Test
-    public void givenMessage_WhenProper_ShouldReturnHappy() {
+    public void givenMessage_WhenProper_ShouldReturnHappy() throws MoodAnalyserException {
         MoodAnalyser moodAnalyser = new MoodAnalyser("User is Happy.");
         String result = moodAnalyser.moodAnalyse();
         System.out.println(result);
         Assertions.assertEquals("HAPPY", result);
     }
     @Test
-    public void givenNewMessage_WhenProper_ShouldReturnSad() {
+    public void givenNewMessage_WhenProper_ShouldReturnSad() throws MoodAnalyserException {
         MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Sad mood.");
         String result = moodAnalyser.moodAnalyse();
         System.out.println(result);
         Assertions.assertEquals("SAD", result);
     }
     @Test
-    public void givenNewMessage_WhenProper_ShouldReturnHappy() {
+    public void givenNewMessage_WhenProper_ShouldReturnHappy() throws MoodAnalyserException {
         MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Any mood.");
         String result = moodAnalyser.moodAnalyse();
         System.out.println(result);
         Assertions.assertEquals("HAPPY", result);
     }
+//    @Test
+//    public void givenNullMessage_WhenProper_ShouldReturnHappy() throws MoodAnalyserException {
+//        MoodAnalyser moodAnalyser = new MoodAnalyser(null);
+//        String result = moodAnalyser.moodAnalyse();
+//        System.out.println(result);
+//        Assertions.assertEquals("HAPPY", result);
+//    }
     @Test
-    public void givenNullMessage_WhenProper_ShouldReturnHappy() {
+    public void givenNullMessage_WhenProper_ShouldReturnExceptionHandled() throws MoodAnalyserException {
         MoodAnalyser moodAnalyser = new MoodAnalyser(null);
-        String result = moodAnalyser.moodAnalyse();
-        System.out.println(result);
-        Assertions.assertEquals("Happy", result);
+        try {
+            String result = moodAnalyser.moodAnalyse();
+            Assertions.assertEquals("HAPPY", result);
+        }
+        catch (MoodAnalyserException e){
+            System.out.println(e.getMessage());
+        }
     }
 }
